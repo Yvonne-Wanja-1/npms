4f578a5341b7adfc726bb04457ca66519dc4f8b8
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-11-25 00:03:36 +0300

    made changes

--- expresss.js
+++ expresss.js
@@ -5,6 +5,13 @@ app.get("/", (req, res) => {
   res.send("Hello Yvonne!");
 });
 
+// Start server
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+// Start server on port 8080
+
 app.listen(8080, () => {
   console.log("Server running on port 3000");
 });
